097a52047fb8f032656540930b399563d4d6f80e
Ojas <ojas1807@gmail.com>
2025-03-24 22:48:45 -0400

    i think i did somethings

--- src/program.rs
+++ src/program.rs
@@ -3,7 +3,7 @@ use std::io::{Read, Write};
 use crate::args::Args;
 
 pub fn run<R: Read, W: Write>(reader: R, mut writer: W, args: Args) {
-    todo!("Write the program logic here");
+    println!("Hello world here there's going to be something");
 }
 
 #[cfg(test)]
